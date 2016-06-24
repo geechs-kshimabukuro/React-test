1d43862f48b85685c67122a598516e634e2e32dc
kabotya <kiyono.shima@gmail.com>
2016-06-24 12:23:53 +0900

    create gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+
+gulp.task('browserify', function() {
+	browserify('./**/*.jsx', { debug: true })
+		.transform(babelify,{presets: ["es2015","react"]})
+		.bundle()
+		.on("error", function (err) { console.log("Error : " + err.message); })
+		.pipe(source('app.js'))
+		.pipe(gulp.dest('./js/'))
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./src/*.jsx', ['browserify'])
+});
+
+gulp.task('default', ['browserify', 'watch']);
